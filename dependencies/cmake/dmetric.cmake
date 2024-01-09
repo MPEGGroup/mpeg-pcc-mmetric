9c2eacec69600a667f2c1744659b52bffc5befcd
marviej <jean-eudes.marvie@interdigital.com>
2024-01-09 09:55:49 +0100

    Fix: path to mpeg git server (attention 1_1_4 retaged again)
    url change due to server migration

--- dependencies/cmake/dmetric.cmake
+++ dependencies/cmake/dmetric.cmake
@@ -5,7 +5,7 @@ include( ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/cmake/CPM.cmake )
 set( DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/dmetric )
 if( NOT EXISTS ${DIR} )
   CPMAddPackage( NAME             dmetric
-                GIT_REPOSITORY    https://mpeg.expert/software/MPEG/3dgh/v-pcc/software/mpeg-pcc-dmetric.git
+                GIT_REPOSITORY    https://git.mpeg.expert/MPEG/3dgh/v-pcc/software/mpeg-pcc-dmetric.git
                 GIT_TAG           release-v0.14.1
                 SOURCE_DIR        ${DIR}
                 DOWNLOAD_ONLY     YES )
